42cdf432e41bbfa5470d0d296829293cabd9ec10
OneButtonDeveloper <one.button.developer@gmail.com>
2015-10-31 15:15:34 +0100

    Added possibility to print logcat in Logger.

--- infrared/src/main/java/com/obd/infrared/log/Logger.java
+++ infrared/src/main/java/com/obd/infrared/log/Logger.java
@@ -1,5 +1,10 @@
 package com.obd.infrared.log;
 
+import android.os.Handler;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public abstract class Logger {
     protected final String tag;
 
@@ -12,4 +17,24 @@ public abstract class Logger {
     public abstract void warning(String message);
 
     public abstract void error(String description, Exception exception);
+
+    public void printLogCat(int delay) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Process process = Runtime.getRuntime().exec("logcat -d");
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        log(line);
+                    }
+                } catch (Exception e) {
+                    error("StartWriteLogs", e);
+                }
+            }
+        }, delay);
+    }
+
 }
